7d4b7448b59d7cbfd5c0efab2111a4098588463a
Alex Bird <horoshiy.ab@gmail.com>
2025-02-21 15:40:20 +0200

    add navbar & sidebar + clerk init

---
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { UserCircleIcon } from "lucide-react";
+import { UserButton, SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+
+export const AuthButton = () => {
+  return (
+    <>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="rounded-full border-blue-500/20 px-4 py-2 text-sm font-medium text-blue-600 shadow-none hover:text-blue-500 [&_svg]:size-4">
+            <UserCircleIcon />
+            Sign In
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
+  );
+};
