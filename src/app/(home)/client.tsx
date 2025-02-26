6324d82c91f14e5cf6705186e6ec90fce6f94cc4
Alex Bird <horoshiy.ab@gmail.com>
2025-02-26 18:34:58 +0200

    seed categories

--- src/app/(home)/client.tsx
+++ src/app/(home)/client.tsx
@@ -3,7 +3,7 @@
 import { trpc } from "@/trpc/client";
 
 export const PageClient = () => {
-  const [data] = trpc.hello.useSuspenseQuery({ text: "Alex" });
+  const [data] = trpc.categories.getMany.useSuspenseQuery();
 
-  return <div>Page Client says: {data.greeting}</div>;
+  return <div>{JSON.stringify(data)}</div>;
 };
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,7 +6,7 @@ import { HydrateClient, trpc } from "@/trpc/server";
 import { PageClient } from "./client";
 
 export default async function Home() {
-  void trpc.hello.prefetch({ text: "Alex" });
+  void trpc.categories.getMany.prefetch();
 
   return (
     <HydrateClient>
