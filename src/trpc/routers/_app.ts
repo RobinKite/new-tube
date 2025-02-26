ea028afd10802d9ff8f95e609df47e59fed91c16
Alex Bird <horoshiy.ab@gmail.com>
2025-02-26 14:53:03 +0200

    add auth and rate limiting to trpc

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,16 +1,15 @@
 import { z } from "zod";
-// import { TRPCError } from "@trpc/server";
 
-import { baseProcedure, createTRPCRouter } from "../init";
+import { createTRPCRouter, protectedProcedure } from "../init";
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
+  hello: protectedProcedure
     .input(
       z.object({
         text: z.string(),
       })
     )
     .query((opts) => {
-      // throw new TRPCError({ code: "BAD_REQUEST", message: "bad request" });
+      console.log({ dbUser: opts.ctx.user });
       return {
         greeting: `hello ${opts.input.text}`,
       };
