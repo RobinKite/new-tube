880be9f807c5b0d3356883e901dbaaea53bd92f0
Alex Bird <horoshiy.ab@gmail.com>
2025-03-04 14:53:05 +0200

    add thumnail, preview and duration to videos in db

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,6 +4,7 @@ import {
   text,
   timestamp,
   uniqueIndex,
+  integer,
 } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 
@@ -51,6 +52,10 @@ export const videos = pgTable("videos", {
   muxPlaybackId: text("mux_playback_id").unique(),
   muxTrackId: text("mux_track_id").unique(),
   muxTrackStatus: text("mux_track_status"),
+
+  thumbnailUrl: text("thumbnail_url"),
+  previewUrl: text("preview_url"),
+  duration: integer("duration"),
 
   userId: uuid("user_id")
     .references(() => users.id, { onDelete: "cascade" })
