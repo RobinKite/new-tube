0b4d4d7f602e74c076e2d5b92c8ce9e2d9873bc5
Alex Bird <horoshiy.ab@gmail.com>
2025-03-05 14:55:23 +0200

    swap string to constant

--- src/modules/videos/ui/components/video-thumbnail.tsx
+++ src/modules/videos/ui/components/video-thumbnail.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 import { formatDuration } from "@/lib/utils";
 
+import { THUMBNAIL_FALLBACK } from "../../constants";
+
 interface VideoThumbnailProps {
   title: string;
   imageUrl?: string | null;
@@ -19,14 +21,14 @@ export const VideoThumbnail = ({
     <div className="group relative">
       <div className="relative aspect-video w-full overflow-hidden rounded-xl">
         <Image
-          src={imageUrl ?? "/placeholder.svg"}
+          src={imageUrl || THUMBNAIL_FALLBACK}
           alt={title}
           fill
           className="size-full object-cover group-hover:opacity-0"
         />
         <Image
           unoptimized={!!previewUrl}
-          src={previewUrl ?? "/placeholder.svg"}
+          src={previewUrl || THUMBNAIL_FALLBACK}
           alt={title}
           fill
           className="size-full object-cover opacity-0 group-hover:opacity-100"
