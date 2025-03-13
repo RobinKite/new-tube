b6cc5883896b5d888bde89cecdbd89d0478f92ed
Alex Bird <horoshiy.ab@gmail.com>
2025-03-13 10:59:38 +0200

    add comment form

---
+++ src/modules/comments/ui/components/comment-form.tsx
@@ -0,0 +1,98 @@
+import { z } from "zod";
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useUser, useClerk } from "@clerk/nextjs";
+
+import { trpc } from "@/trpc/client";
+import { commentInsertSchema } from "@/db/schema";
+import { Textarea } from "@/components/ui/textarea";
+import { UserAvatar } from "@/components/user-avatar";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+
+interface CommentFormProps {
+  videoId: string;
+  onSuccess?: () => void;
+}
+
+export const CommentForm = ({ videoId, onSuccess }: CommentFormProps) => {
+  const { user } = useUser();
+
+  const clerk = useClerk();
+  const utils = trpc.useUtils();
+  const create = trpc.comments.create.useMutation({
+    onSuccess: () => {
+      utils.comments.getMany.invalidate({ videoId });
+      form.reset();
+      toast.success("Comment added");
+      onSuccess?.();
+    },
+    onError: (error) => {
+      toast.error(error.message);
+
+      if (error.data?.code === "UNAUTHORIZED") clerk.openSignIn();
+    },
+  });
+
+  const commentSchema = commentInsertSchema.omit({ userId: true });
+
+  const form = useForm<z.infer<typeof commentSchema>>({
+    resolver: zodResolver(commentSchema),
+    defaultValues: {
+      videoId,
+      value: "",
+    },
+  });
+
+  const handleSubmit = (values: z.infer<typeof commentSchema>) => {
+    create.mutate(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="group flex gap-4">
+        <UserAvatar
+          size="lg"
+          imageUrl={user?.imageUrl || "/user-placeholder.svg"}
+          name={user?.username || "User"}
+        />
+        <div className="flex-1">
+          <FormField
+            name="value"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Textarea
+                    {...field}
+                    placeholder="Add a comment..."
+                    className="min-h-0 resize-none overflow-hidden bg-transparent"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="mt-2 flex justify-end gap-2">
+            <Button
+              disabled={create.isPending}
+              type="submit"
+              size="sm">
+              Comment
+            </Button>
+          </div>
+        </div>
+      </form>
+    </Form>
+  );
+};
