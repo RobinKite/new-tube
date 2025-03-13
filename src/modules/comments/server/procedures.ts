11c283529b1b5477fbb36cbbbd0016ab111d1179
Alex Bird <horoshiy.ab@gmail.com>
2025-03-13 13:35:21 +0200

    add infinite scroll procedure to comments

--- src/modules/comments/server/procedures.ts
+++ src/modules/comments/server/procedures.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { eq, getTableColumns } from "drizzle-orm";
+import { and, count, desc, eq, getTableColumns, lt, or } from "drizzle-orm";
 
 import {
   baseProcedure,
@@ -24,16 +24,55 @@ export const commentsRouter = createTRPCRouter({
       return createdComment;
     }),
   getMany: baseProcedure
-    .input(z.object({ videoId: z.string().uuid() }))
+    .input(
+      z.object({
+        videoId: z.string().uuid(),
+        cursor: z
+          .object({ id: z.string().uuid(), updatedAt: z.date() })
+          .nullish(),
+        limit: z.number().min(1).max(100),
+      })
+    )
     .query(async ({ input }) => {
-      const { videoId } = input;
+      const { videoId, cursor, limit } = input;
 
-      const data = await db
-        .select({ ...getTableColumns(comments), user: users })
-        .from(comments)
-        .where(eq(comments.videoId, videoId))
-        .innerJoin(users, eq(comments.userId, users.id));
+      const [totalData, data] = await Promise.all([
+        db
+          .select({ count: count() })
+          .from(comments)
+          .where(eq(comments.videoId, videoId)),
+        db
+          .select({
+            ...getTableColumns(comments),
+            user: users,
+          })
+          .from(comments)
+          .where(
+            and(
+              eq(comments.videoId, videoId),
+              cursor
+                ? or(
+                    lt(comments.updatedAt, cursor.updatedAt),
+                    and(
+                      eq(comments.updatedAt, cursor.updatedAt),
+                      lt(comments.id, cursor.id)
+                    )
+                  )
+                : undefined
+            )
+          )
+          .innerJoin(users, eq(comments.userId, users.id))
+          .orderBy(desc(comments.updatedAt), desc(comments.id))
+          .limit(limit + 1),
+      ]);
 
-      return data;
+      const hasMore = data.length > limit;
+      const items = hasMore ? data.slice(0, -1) : data;
+      const lastItem = items[items.length - 1];
+      const nextCursor = hasMore
+        ? { id: lastItem.id, updatedAt: lastItem.updatedAt }
+        : null;
+
+      return { items, nextCursor, totalCount: totalData[0].count };
     }),
 });
