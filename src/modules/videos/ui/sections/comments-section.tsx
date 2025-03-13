09a2f0078c36f3020eabde13b479b1376a0e0f9a
Alex Bird <horoshiy.ab@gmail.com>
2025-03-13 13:35:35 +0200

    add infinite scroll usage to comments

--- src/modules/videos/ui/sections/comments-section.tsx
+++ src/modules/videos/ui/sections/comments-section.tsx
@@ -6,6 +6,8 @@ import { ErrorBoundary } from "react-error-boundary";
 import { trpc } from "@/trpc/client";
 import { CommentForm } from "@/modules/comments/ui/components/comment-form";
 import { CommentItem } from "@/modules/comments/ui/components/comment-item";
+import { DEFAULT_LIMIT } from "@/constants";
+import { InfiniteScroll } from "@/components/infinite-scroll";
 
 interface CommentsSectionProps {
   videoId: string;
@@ -22,20 +24,33 @@ export const CommentsSection = ({ videoId }: CommentsSectionProps) => {
 };
 
 export const CommentsSectionSuspense = ({ videoId }: CommentsSectionProps) => {
-  const [comments] = trpc.comments.getMany.useSuspenseQuery({ videoId });
+  const [comments, query] = trpc.comments.getMany.useSuspenseInfiniteQuery(
+    { videoId, limit: DEFAULT_LIMIT },
+    { getNextPageParam: (lastPage) => lastPage.nextCursor }
+  );
 
   return (
     <div className="mt-6">
       <div className="flex flex-col gap-6">
-        <h2>0 comments</h2>
+        <h2 className="text-xl font-bold">
+          {comments.pages[0].totalCount} comments
+        </h2>
         <CommentForm videoId={videoId} />
         <div className="mt-2 flex flex-col gap-4">
-          {comments.map((comment) => (
-            <CommentItem
-              key={comment.id}
-              comment={comment}
-            />
-          ))}
+          {comments.pages
+            .flatMap((page) => page.items)
+            .map((comment) => (
+              <CommentItem
+                key={comment.id}
+                comment={comment}
+              />
+            ))}
+          <InfiniteScroll
+            isManual
+            hasNextPage={query.hasNextPage}
+            isFetchingNextPage={query.isFetchingNextPage}
+            fetchNextPage={query.fetchNextPage}
+          />
         </div>
       </div>
     </div>
