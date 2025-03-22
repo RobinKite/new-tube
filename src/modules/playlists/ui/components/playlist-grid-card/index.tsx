cbd3e4db8c8b6c21c5cc3bb121f464105c302668
Alex Bird <horoshiy.ab@gmail.com>
2025-03-22 23:26:49 +0200

    add playlist grid card

---
+++ src/modules/playlists/ui/components/playlist-grid-card/index.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import { Route } from "next";
+
+import {
+  PlaylistThumbnail,
+  PlaylistThumbnailSkeleton,
+} from "./playlist-thumbnail";
+import { PlaylistInfo, PlaylistInfoSkeleton } from "./playlist-info";
+
+import { PlaylistGetManyOutput } from "@/modules/playlists/types";
+import { THUMBNAIL_FALLBACK } from "@/modules/videos/constants";
+
+interface PlaylistGridCardProps {
+  data: PlaylistGetManyOutput["items"][number];
+}
+
+export const PlaylistGridCardSkeleton = () => {
+  return (
+    <div className="flex w-full flex-col gap-2">
+      <PlaylistThumbnailSkeleton />
+      <PlaylistInfoSkeleton />
+    </div>
+  );
+};
+
+export const PlaylistGridCard = ({ data }: PlaylistGridCardProps) => {
+  return (
+    <Link href={`/playlists/${data.id}` as Route}>
+      <div className="group flex w-full flex-col gap-2">
+        <PlaylistThumbnail
+          imageURL={THUMBNAIL_FALLBACK}
+          title={data.name}
+          videoCount={data.videoCount}
+        />
+        <PlaylistInfo data={data} />
+      </div>
+    </Link>
+  );
+};
