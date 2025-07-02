cb31d480cc6aa1351db3371b21066d599ce49e0e
Alex Bird <horoshiy.ab@gmail.com>
2025-07-02 18:09:32 +0300

    add user banner

---
+++ src/modules/users/ui/components/user-page-banner.tsx
@@ -0,0 +1,39 @@
+import { useAuth } from "@clerk/nextjs";
+import { Edit2Icon } from "lucide-react";
+
+import { UserGetOneOutput } from "../../types";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+interface UserPageBannerProps {
+  user: UserGetOneOutput;
+}
+
+export const UserPageBanner = ({ user }: UserPageBannerProps) => {
+  const { userId } = useAuth();
+
+  return (
+    <div className="group relative">
+      <div
+        className={cn(
+          "w-full max-h-[200px] h-[15vh] md:h-[25vh] bg-gradient-to-r from-gray-100 to-gray-200 rounded-xl",
+          user.bannerUrl ? "bg-cover bg-center" : "bg-gray-100"
+        )}
+        style={{
+          backgroundImage: user.bannerUrl
+            ? `url(${user.bannerUrl})`
+            : undefined,
+        }}>
+        {user.clerkId === userId && (
+          <Button
+            type="button"
+            size="icon"
+            className="absolute right-4 top-4 rounded-full bg-black/50 opacity-100 transition-opacity duration-300 hover:bg-black/50 group-hover:opacity-100 md:opacity-0">
+            <Edit2Icon className="size-4 text-white" />
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
