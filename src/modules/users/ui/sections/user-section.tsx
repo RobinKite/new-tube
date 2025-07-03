e6b0fee84bac54f812c5cf3df87a0af6d7643e51
Alex Bird <horoshiy.ab@gmail.com>
2025-07-03 14:59:34 +0300

    add user section skeleton

--- src/modules/users/ui/sections/user-section.tsx
+++ src/modules/users/ui/sections/user-section.tsx
@@ -3,9 +3,17 @@
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
-import { UserPageBanner } from "../components/user-page-banner";
-
 import { trpc } from "@/trpc/client";
+import { Separator } from "@/components/ui/separator";
+
+import {
+  UserPageBanner,
+  UserPageBannerSkeleton,
+} from "../components/user-page-banner";
+import {
+  UserPageInfo,
+  UserPageInfoSkeleton,
+} from "../components/user-page-info";
 
 interface UserSectionProps {
   userId: string;
@@ -13,11 +21,21 @@ interface UserSectionProps {
 
 export const UserSection = (props: UserSectionProps) => {
   return (
-    <Suspense fallback={"Loading..."}>
+    <Suspense fallback={<UserSectionSkeleton />}>
       <ErrorBoundary fallback={"Error"}>
         <UserSectionSuspense {...props} />
       </ErrorBoundary>
     </Suspense>
+  );
+};
+
+const UserSectionSkeleton = () => {
+  return (
+    <div>
+      <UserPageBannerSkeleton />
+      <UserPageInfoSkeleton />
+      <Separator />
+    </div>
   );
 };
 
@@ -27,6 +45,8 @@ const UserSectionSuspense = ({ userId }: UserSectionProps) => {
   return (
     <div className="flex flex-col">
       <UserPageBanner user={user} />
+      <UserPageInfo user={user} />
+      <Separator />
     </div>
   );
 };
